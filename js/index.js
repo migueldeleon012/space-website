69e9cadfffe343f9aa133243dfb371ab9dc99ce3
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2022-08-25 21:58:37 +0200

    fix: active destination not changing classname

--- js/index.js
+++ js/index.js
@@ -15,7 +15,9 @@ closeNavButton.addEventListener('click', () => {
 //from here on out there's data handling for the pages
 import data from './data.json' assert { type: 'json' };
 
-//destinations
+//DESTINATIONS HANDLING
+
+//destinations html
 const destinationHeading = document.querySelector('#destinationHeading');
 const destinationDescription = document.querySelector(
   '#destinationDescription'
@@ -24,21 +26,29 @@ const destinationDistance = document.querySelector('#destinationDistance');
 const destinationTime = document.querySelector('#destinationTime');
 const destinationsImage = document.querySelector('.destinations__img img');
 const destinations = document.querySelectorAll('.destination');
-//data needed for the destinations
+const destinationTextColor = document.querySelectorAll('.destination-text');
+let destinationActive = document.querySelector('.destination.active');
+//get the data of the destinations
 const destinationData = data.data.destinations;
-
-console.log(destinations);
 
 destinations.forEach((destination, index) => {
   destination.addEventListener('click', () => {
+    //change data
     destinationHeading.innerHTML = destinationData[index].header;
     destinationDescription.innerHTML = destinationData[index].description;
     destinationDistance.innerHTML = destinationData[index].distance;
     destinationTime.innerHTML = destinationData[index].time;
     destinationsImage.src = destinationData[index].img;
-    destinationsImage.alt = destinationData[index].alt;
+    destinationsImage.alt = ` Image: ${destinationData[index].header}`;
+    //change active line
+    destinationActive.classList.remove('active');
+    destination.classList.add('active');
+    destinationActive = document.querySelector('.destination.active');
+    //change active color
+    destinationTextColor.forEach((text) => text.classList.add('light-blue'));
+    destinationTextColor[index].classList.contains('light-blue')
+      ? destinationTextColor[index].classList.remove('light-blue')
+      : null;
   });
   return;
 });
-
-data.data.destinations.map((item) => console.log(item));
